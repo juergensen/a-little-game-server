5be5497a30be883073b20cef57d6a21358dc34cc
Momme Jürgensen <momme@juergensen.me>
2016-06-20 21:08:39 +0200

    multiplayer (many bugs)

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log("Hello World")
+
+
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var Game = require('./lib/game.js');
+var game = new Game(io)
+
+app.get('/start/countdown/:startNumber?', function(req, res){
+  countdown(req.params.startNumber)
+  res.send({status:"ok", startNumber:req.params.startNumber})
+});
+
+app.get('/start/:reason?/:color?', function(req, res){
+  game.startGame((reason) => {
+    res.send(reason)
+  },req.params.reason, req.params.color)
+});
+app.get('/stop/:reason?/:color?', function(req, res){
+  game.stopGame((reason) => {
+    res.send(reason)
+  },req.params.reason, req.params.color)
+});
+
+
+function countdown(startNumber) {
+  game.stopGame((reason) => {},startNumber, "green")
+  var startNumber = startNumber || 3
+  var countdown = setInterval(function () {
+    if (startNumber <= 0) {
+      game.startGame((reason) => {},"Loos", "green")
+      clearInterval(countdown);
+    } else {
+      game.stopGame((reason) => {},startNumber, "green")
+    }
+    startNumber--;
+  }, 1000)
+}
+
+
+http.listen(9001, function(){
+  console.log('listening on *:9001');
+});
